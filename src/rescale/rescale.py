c9096fef03a450a233c6f3bb0762da0c857c5a35
evamaxfield <evamaxfieldbrown@gmail.com>
2024-06-25 15:05:46 -0400

    Linted the code!

--- src/rescale/rescale.py
+++ src/rescale/rescale.py
@@ -7,9 +7,6 @@ def rescale(input_array):
     Takes an array as input, and returns a corresponding array scaled
     so that 0 corresponds to the minimum and 1 to the maximum value
     of the input array.
-
-    Parameters
-    ----------
     """
     L = np.min(input_array)
     H = np.max(input_array)
